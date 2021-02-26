eb0e8e0ed560d0455006f2e019ba61a4e8729f5a
Aguinaldo Bezerra <aguinaldoabbj@users.noreply.github.com>
2021-02-26 18:36:53 -0300

    Update gituserconf.sh

--- gituserconf.sh
+++ gituserconf.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -rf /home/jovyan/.jupyter/jupyter_notebook_config.json
+
 echo "teste" > /home/jovyan/teste.txt
 git config --system user.name $JUPYTERHUB_USER
 git config --system user.email $JUPYTERHUB_USER@example.com
